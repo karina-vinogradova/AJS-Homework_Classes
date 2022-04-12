7b18bc5e86f2ac2f13bc95069caee356aad60493
Karina Vinogradova <x-mower@mail.ru>
2022-04-12 19:40:23 +0300

    Webpack built

--- src/index.js
+++ src/index.js
@@ -1,15 +1,7 @@
-import Character from './character';
-import Bowman from './bowman';
-import Daemon from './daemon';
-import Magician from './magician';
-import Swordsman from './swordsman';
-import Undead from './undead';
-import Zombie from './zombie';
-
-console.log(new Bowman('Bob'));
-console.log(new Daemon('Dwarf'));
-console.log(new Magician('Martin'));
-console.log(new Swordsman('Simon'));
-console.log(new Undead('Ugly'));
-console.log(new Zombie('Zina'));
-console.log(new Character('Calvin'));
+import './character';
+import './bowman';
+import './daemon';
+import './magician';
+import './swordsman';
+import './undead';
+import './zombie';
